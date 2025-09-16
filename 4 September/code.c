c2356e765e8d3b77ca6ae6a7fdf6d21873273a8c
meetsanghani138 <meetsanghani0311@gmail.com>
2025-09-16 19:09:18 +0530

    leetcode -solution -4

---
+++ 4 September/code.c
@@ -0,0 +1,10 @@
+int findClosest(int x, int y, int z) {
+    int dxz = abs(x - z), dyz = abs(y - z);
+    if (dxz < dyz) {
+        return 1;
+    } else if (dxz > dyz) {
+        return 2;
+    } else {
+        return 0;
+    }
+}
